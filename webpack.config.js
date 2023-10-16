878d241101feef1efa97996665849ca91203858c
Ameer Ameen <ameerapex496@gmail.com>
2023-10-16 02:08:22 +0200

    switched webpack, to development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "prodoction",
+    mode: "development",
     entry: {
         bundle: path.resolve(__dirname, "src", "index.js"),
     },
